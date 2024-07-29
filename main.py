3c29e3fa6435c23d9d8f0617444db92f8b98a22c
dimaffffff <97465037+dimaffffff@users.noreply.github.com>
2024-07-29 17:48:21 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+list1 = ["#","#","#","#","#","#","#",]
+list2 = ["#","#","#","#","#","#","#",]
+list3 = ["#","#","#","#","#","#","#",]
+list4 = ["#","#","#","#","#","#","#",]
+list5 = ["#","#","#","#","#","#","#",]
+list6 = ["#","#","#","#","#","#","#",]
+list7 = ["#","#","#","#","#","#","#",]
+
+
+time = 25
+int(time)
+while True:
+    if time == 25:
+        os.system("cls||clear")
+        print(list1)
+        print(list2)
+        print(list3)
+        print(list4)
+        print(list5)
+        print(list6)
+        print(list7)
+        time = 0
+
+    else:
+        time = time + 1
